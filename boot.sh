5dceaac40e09aef0de65ae5b38cd2728f094c073
abulanov <anbulano@gmail.com>
2020-07-02 01:16:38 -0700

    priority options added to ovs-ctl

--- boot.sh
+++ boot.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-#export PATH=$PATH:/usr/share/openvswitch/scripts/ &&\
-ovs-ctl start &&\
+ovs-ctl --ovsdb-server-priority=0 --ovs-vswitchd-priority=0 start &&\
 ovs-vsctl add-br lan-br &&\
 ovs-vsctl add-port lan-br eth0 &&\
 PORT_IP=$(ip add show dev eth0 | grep inet | awk '{print $2}') &&\
@@ -10,9 +9,7 @@ ip ad add $PORT_IP dev lan-br &&\
 ip link set lan-br up &&\
 ip route add default via $GW_IP dev lan-br &&\
 ovs-vsctl set-controller lan-br tcp:$GW_IP:6633 &&\
-sleep 5 &&\
-echo "Registring" &&\
+echo "Registering" &&\
 /json_register.py $@
-
 
 /bin/sh
